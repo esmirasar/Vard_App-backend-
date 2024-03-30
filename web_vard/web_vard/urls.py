f04fb1d31f884677f82e7bc516748fc712de763c
Andrey <v.andrei.dmitrievich@gmail.com>
2024-03-30 15:48:55 +0300

    setting djoser jwt

--- web_vard/web_vard/urls.py
+++ web_vard/web_vard/urls.py
@@ -17,7 +17,6 @@ urlpatterns = [
 
     path('api/connection/', include('local_db.urls')),
 
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
+    path(r'^auth/', include('djoser.urls')),
+    path(r'^auth/', include('djoser.urls.jwt'))
 ]
