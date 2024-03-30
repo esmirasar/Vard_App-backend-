2670ba2cd6fc7ee06c63c16722923255fd892476
Andrey <v.andrei.dmitrievich@gmail.com>
2024-03-30 21:03:28 +0300

    test

--- web_vard/access/urls.py
+++ web_vard/access/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path('all/', ListAccessAPIView.as_view()),
     path('<str:my_access>/', GetPostAccessAPIView.as_view()),
     path('<str:my_access>/<int:pk>/', PutDeleteAccessAPIView.as_view()),
-    path('auth/', include('djoser.urls')),
-    re_path(r'^auth/', include('djoser.urls.authtoken')),
 ]
