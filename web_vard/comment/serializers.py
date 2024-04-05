20e7a5ee4ef410fed233e82ba61d8d98f6e5f673
esmiraSar <esmirasar@yandex.ru>
2024-04-05 20:25:00 +0300

    Comment serializer

--- web_vard/comment/serializers.py
+++ web_vard/comment/serializers.py
@@ -10,24 +10,21 @@ from user.models import User
 class CommentSerializer(serializers.Serializer):
 
     id = serializers.IntegerField(read_only=True)
-    file = serializers.PrimaryKeyRelatedField(queryset=File.objects.all())
-    chart = serializers.PrimaryKeyRelatedField(queryset=Chart.objects.all())
-    dashboard = serializers.PrimaryKeyRelatedField(queryset=Dashboard.objects.all())
+    file = serializers.PrimaryKeyRelatedField(queryset=File.objects.all(), allow_null=True, allow_empty=True)
+    chart = serializers.PrimaryKeyRelatedField(queryset=Chart.objects.all(), allow_null=True, allow_empty=True)
+    dashboard = serializers.PrimaryKeyRelatedField(queryset=Dashboard.objects.all(), allow_null=True, allow_empty=True)
     user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
-    date_send = serializers.DateTimeField(read_only=True)
-    date_remove = serializers.DateTimeField(read_only=True)
-    date_delivery = serializers.DateTimeField(read_only=True)
-    comment = serializers.CharField()
+    date_send = serializers.DateTimeField(allow_null=True )
+    date_remove = serializers.DateTimeField(allow_null=True)
+    date_delivery = serializers.DateTimeField(allow_null=True)
+    comment = serializers.CharField(allow_null=True, allow_blank=True)
 
     def create(self, validated_data):
         return Comment.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
 
-        instance.file = validated_data.get('file', instance.file)
-        instance.dashboard = validated_data.get('dashboard', instance.dashboard)
-        instance.chart = validated_data.get('chart', instance.chart)
-        instance.comment = validated_data.get('comment', instance.comment)
+        instance.comment = validated_data.get('description', instance.comment)
         instance.save()
 
         return instance
